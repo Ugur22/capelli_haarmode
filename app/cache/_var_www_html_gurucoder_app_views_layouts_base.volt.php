a3d9c94f17805fe19827b43ea00cc89225939367
ugur <ugurertas9@gmail.com>
2016-01-06 13:55:05 +0100

    alles

---
+++ app/cache/_var_www_html_gurucoder_app_views_layouts_base.volt.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php echo $this->tag->getTitle(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" type="image/png" href="http://www.clicinterieurconcepten.nl/img/morebyme-50x50.jpg"/>
+    <?php echo $this->assets->outputCss('header'); ?>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <base href="index">
+</head>
+<body>
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper">
+            <span class="brand-logo right">Capelli Haarmode</span>
+            <a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="left hide-on-med-and-down">
+                <li><a   href="<?php echo $this->url->get('index'); ?>">home</a></li>
+                <li id="about"><a href="<?php echo $this->url->get('overons'); ?>">over ons</a></li>
+                <li><a href="<?php echo $this->url->get('contact'); ?>">contact</a></li>
+                <li><a href="<?php echo $this->url->get('account'); ?>">Login</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="<?php echo $this->url->get('index'); ?>">home</a></li>
+                <li><a href="<?php echo $this->url->get('overons'); ?>">over ons</a></li>
+                <li><a href="<?php echo $this->url->get('contact'); ?>">contact</a></li>
+                <li><a href="<?php echo $this->url->get('account'); ?>">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<!--<h1><?php echo $this->dispatcher->getActionName(); ?></h1>-->
+<article>
+    
+    
+</article>
+<?php echo $this->assets->outputJs('footer'); ?>
+</body>
+</html>
